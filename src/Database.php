9547dfff79c2416d8d35769e4bf32ee804039b17
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-01-22 21:18:57 +0700

    Changelog 1.0.14: - Fixed results() function should return array instead documents

--- src/Database.php
+++ src/Database.php
@@ -15,7 +15,7 @@ class Database
     * Stores the version of Filebase
     * use $db->getVersion()
     */
-    const VERSION = '1.0.12';
+    const VERSION = '1.0.14';
 
 
     //--------------------------------------------------------------------
